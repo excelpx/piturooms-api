6076cab5c30dcdc67d8314a735deddb55c665671
Mr.Ex <100412505+excelpx@users.noreply.github.com>
2026-07-07 20:51:17 +0700

    Add files via upload

---
+++ send-invoice.js
@@ -0,0 +1,51 @@
+export default async function handler(req, res) {
+ if(req.method !== "POST"){
+  return res.status(405).json({
+   error:"Method not allowed"
+  });
+ }
+ try{
+  const {
+   email,
+   bookingCode,
+   invoiceHTML
+  } = req.body;
+  const response =
+  await fetch(
+   "https://api.resend.com/emails",
+   {
+    method:"POST",
+    headers:{
+     "Authorization":
+     `Bearer ${process.env.re_eqniK4sj_Pd5MzSDWJN5TrnChQaEYBa6b}`,
+     "Content-Type":
+     "application/json"
+    },
+
+
+    body:JSON.stringify({
+     from:
+     "Piturooms Hotel <onboarding@resend.dev>",
+     to:
+     email,
+     subject:
+     `Invoice Booking ${bookingCode}`,
+     html:
+     invoiceHTML
+    })
+   }
+  );
+  const data =
+  await response.json();
+  return res.json({
+   success:true,
+   data
+  });
+ }catch(error){
+  return res.status(500)
+  .json({
+   success:false,
+   error:error.message
+  });
+ }
+}
